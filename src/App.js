8da59f468cd1653a9259b83577dde825bd02bf88
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-05-07 08:36:08 +0000

    update

--- src/App.js
+++ src/App.js
@@ -12,12 +12,21 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
   console.log(notification);
   useEffect(() => {
-    const sendCartData = async () => {};
-    fetch("https://rjs-20-1cc19-default-rtdb.firebaseio.com/cart.json", {
-      method: "PUT",
-      body: JSON.stringify(cart),
+    const sendCartData = async () => {
+      const response = await fetch(
+        "https://rjs-20-1cc19-default-rtdb.firebaseio.com/cart.json",
+        {
+          method: "PUT",
+          body: JSON.stringify(cart),
+        },
+      );
+      if (!response.ok) {
+        throw new Error("Sending cart data failed.");
+      }
+    };
+    sendCartData().catch((error) => {
+      console.log("false send Cart Data!!!");
     });
-    console.log("da fetch cart nha!!!");
   }, [cart]);
 
   return (
