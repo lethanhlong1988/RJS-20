820765b3d28171151f03eb8a5a62df4eff56ea55
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-05-07 07:32:03 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,15 +1,18 @@
 import { useEffect } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import Layout from "./components/Layout/Layout";
 import Cart from "./components/Cart/Cart";
 import Products from "./components/Shop/Products";
 
 function App() {
+  const dispatch = useDispatch();
   const showCart = useSelector((state) => state.ui.cartIsVisible);
   const cart = useSelector((state) => state.cart);
-  console.log(cart);
+  const notification = useSelector((state) => state.ui.notification);
+  console.log(notification);
   useEffect(() => {
+    const sendCartData = async () => {};
     fetch("https://rjs-20-1cc19-default-rtdb.firebaseio.com/cart.json", {
       method: "PUT",
       body: JSON.stringify(cart),
