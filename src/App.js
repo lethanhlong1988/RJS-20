677d11b960894902f113b9bb7549f785c705bb30
long le thanh <61105646+lethanhlong1988@users.noreply.github.com>
2024-05-07 06:17:11 +0000

    update

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import { useEffect } from "react";
 import { useSelector } from "react-redux";
 
 import Layout from "./components/Layout/Layout";
@@ -6,6 +7,16 @@ import Products from "./components/Shop/Products";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+  console.log(cart);
+  useEffect(() => {
+    fetch("https://rjs-20-1cc19-default-rtdb.firebaseio.com/cart.json", {
+      method: "PUT",
+      body: JSON.stringify(cart),
+    });
+    console.log("da fetch cart nha!!!");
+  }, [cart]);
+
   return (
     <Layout>
       {showCart && <Cart />}
